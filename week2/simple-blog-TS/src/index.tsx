1c61fa48d1a2038236bd02b4ae89f5ae2e35be26
Jason Tran <jsontran5@gmail.com>
2025-02-10 05:34:10 -0800

    week3 next.js task

--- week2/simple-blog-TS/src/index.tsx
+++ week2/simple-blog-TS/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BlogProvider } from "./BlogContext.tsx"; // Ensure this import is correct
+import { BlogProvider } from "./BlogContext.tsx"; 
 import reportWebVitals from "./reportWebVitals.js";
 
 const root = ReactDOM.createRoot(
